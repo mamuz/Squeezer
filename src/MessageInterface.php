516578b34e1da71c371eee12a0703168927db7cb
mamuz <mamuz.de@gmail.com>
2015-06-28 19:29:05 +0200

    first commit

--- src/MessageInterface.php
+++ src/MessageInterface.php
@@ -29,7 +29,7 @@ interface MessageInterface
 {
     const VERSION = 'dev-master';
 
-    const NAME = 'Squeezer by Marco Muths';
+    const NAME = 'Squeezer by Dennis Kopp and Marco Muths';
 
     const COMMAND = 'squeeze';
 
